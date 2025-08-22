2228482f07e09172614c975e3a1633b6aa8ff32d
Hussein hijazi <hussein.hijazi04@lau.edu>
2025-08-22 16:06:21 +0300

    the first project submission

---
+++ ex1solved.py
@@ -0,0 +1,11 @@
+name=""
+age =""
+
+# Ask the user about their name
+name = input("What is your name? ")
+
+# Ask the user about their age
+age = input("How old are you? ")
+
+# Print the result
+print(f"Hello {name} , you are {age} years old .")
